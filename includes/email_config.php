08319d9c96ad622d686225b4e5eba0082fb27781
correasamanta12 <correasamanta12@gmail.com>
2025-12-14 13:04:11 -0500

    Update email_config.php

--- includes/email_config.php
+++ includes/email_config.php
@@ -2,6 +2,6 @@
 define('EMAIL_HOST', 'smtp.gmail.com');
 define('EMAIL_PORT', 587);
 define('EMAIL_USERNAME', 'eventure389@gmail.com');
-define('EMAIL_PASSWORD', 'Eventure_389$');
+define('EMAIL_PASSWORD', 'bfzx ypiw hqop snxf');
 define('EMAIL_FROM_ADDRESS', 'eventure389@gmail.com');
 define('EMAIL_FROM_NAME', 'Eventure Event Planner');
